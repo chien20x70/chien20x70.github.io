923e0bbe499159b01b8969f639c6fd87c889260a
chien20x70 <chien20x70@gmail.com>
2023-11-28 22:29:07 +0700

    add wave sound

--- index_files/bii.js
+++ index_files/bii.js
@@ -87,10 +87,12 @@ if(biicore.bgMusic){
 		document.getElementsByClassName("bii-player")[0].classList.remove("show-sec");
 	    if (audioPlayer.paused) {
 			audioPlayer.play();
+			document.getElementById("wave-rage-key").style.display = "flex";
 			document.getElementById("playerVolumeOff").style.display = "none";
 			document.getElementById("playerVolumeOn").style.display = "block";
 		} else {
 			audioPlayer.pause(); 
+			document.getElementById("wave-rage-key").style.display = "none";
 			document.getElementById("playerVolumeOff").style.display = "block";
 			document.getElementById("playerVolumeOn").style.display = "none";
 		}
